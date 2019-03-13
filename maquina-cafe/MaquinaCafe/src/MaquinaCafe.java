3137a97479121f910c9c47ea52e365e9ab81b2f6
Fatima <fatima.carneiro@bluesoft.com.br>
2019-03-13 18:32:31 -0300

    separando as classes

--- maquina-cafe/MaquinaCafe/src/MaquinaCafe.java
+++ maquina-cafe/MaquinaCafe/src/MaquinaCafe.java
@@ -1,4 +1,42 @@
+import java.util.Scanner;
+
 public class MaquinaCafe {
-//    Menu menu = new Menu;
+  public void maquinaCafe (){
+
+         Menu menu = new Menu();
+         menu.geraOpcoes();
+         menu.mostraMenu();
+
+    // ------------------------ AQUI COMEÇA A ESCOLHA DO USUÁRIO
+
+    Scanner pedido = new Scanner(System.in);
+        System.out.println("Olá! Para selecionar uma bebida, digite o número que antecede o nome do produto: ");
+    int escolha = pedido.nextInt();
+
+    //  VALIDAÇÃO
+
+    Opcao opcaoEscolhida = null;
+
+        if (escolha != 1 && escolha != 2 && escolha != 3 && escolha != 4 && escolha != 5) {
+
+        System.out.println("Opção inválida. Favor digitar o número que antecede o produto. Exemplo: para escolher Café, digite 1");
+
+
+    } else {
+        opcaoEscolhida = menu.escolherOpcao(escolha);
+    }
+
+    //------------------   ACABA PEDIDO
+    NivelAcucar nivelAcucar = new NivelAcucar();
+        nivelAcucar.alteraAcucar(escolha, opcaoEscolhida);
+    // ----------------- COBRA
+    Cobranca cobra = new Cobranca();
+        cobra.cobra(opcaoEscolhida);
+
+
+    // ----------------- PREPARO
+    Pedido x = new Pedido();
+        x.prepadaMostraEntregaPedido(opcaoEscolhida);
+}
 
 }
--- maquina-cafe/MaquinaCafe/src/Pedido.java
+++ maquina-cafe/MaquinaCafe/src/Pedido.java
@@ -1,2 +1,107 @@
 public class Pedido {
+
+    public void prepadaMostraEntregaPedido (Opcao opcaoEscolhida) {
+        switch (opcaoEscolhida.codigo){
+            case 1:
+                try {
+                    System.out.println("\n" + "Fervendo a água...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o pó do café no filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Passando a água no filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o café no copo...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o açúcar...");}
+
+                catch (InterruptedException erro){
+                    System.out.println(erro);
+                }
+                break;
+
+            case 2:
+                try {
+                    System.out.println("\n" + "Fervendo a água...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o pó do café no filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Passando a água no filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o café no copo...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o leite no copo...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o açúcar...");
+
+                }
+
+                catch (InterruptedException erro){
+                    System.out.println(erro);
+                }
+                break;
+            case 3:
+                try {
+                    System.out.println("\n" + "Fervendo a água...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o pó do café no filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Passando a água no filtro...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o café no copo...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o leite...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o chocolate...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando a canela em pó...");
+                    Thread.sleep(1000);
+                    System.out.println("Misturando os ingredientes...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o açúcar...");
+                }
+                catch (InterruptedException erro){
+                    System.out.println(erro);
+                }
+                break;
+
+            case 4:
+                try {
+                    System.out.println("\n" + "Fervendo a água...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o chá na água...");
+                    Thread.sleep(3000);
+                    System.out.println("Aguarde...");
+                    Thread.sleep(1000);
+                    System.out.println("Colocando o chá no copo...");
+                    Thread.sleep(1000);
+                    System.out.println("Adicionando o açúcar...");
+                }
+                catch (InterruptedException erro){
+                    System.out.println(erro);
+                }
+                break;
+
+            case 5:
+                try {
+                    System.out.println("\n" + "Fervendo a água...");
+                    Thread.sleep(2000);
+                    System.out.println("Colocando a água quente no copo.");
+                }
+
+                catch (InterruptedException erro){
+                    System.out.println(erro);
+                }
+                break;
+        }
+
+         System.out.println("\n" + "Retire seu " + opcaoEscolhida.descricao);
+
+    }
+
 }
